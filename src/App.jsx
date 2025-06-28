cfb705bcf4fc9e2d4e5a905fbec6de9ddb537ab0
farisazizy289 <farisazizy57@gmail.com>
2025-06-29 04:36:36 +0700

    memperbaiki typo import

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@
 // import Form from "./Pages/Auth/Components/form";
 import LayoutAdmin from "./Pages/Layouts/layoutAdmin";
 import LayoutAuth from "./Pages/Layouts/layoutAuth";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProtectedRoute from './Pages/components/ProtectedRoute';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./Pages/components/ProtectedRoute";
 
-import Dashboard from './Pages/dashboard';
+import Dashboard from "./Pages/Dashboard";
 // import Mahasiswa from './Pages/mahasiswa';
-import Mahasiswa from './Pages/Mahasiswa2';
+import Mahasiswa from "./Pages/Mahasiswa2";
 
 import Login from "./Pages/login";
 import MatkulDetail from "./Pages/MatkulDetail";
@@ -25,51 +25,50 @@ import User from "./Pages/User";
 import RencanaStudi from "./Pages/RencanaStudi";
 
 const App = () => {
-  return (  <>
-    <BrowserRouter>
-    <Routes>
-      {/* Auth Routes */}
-      <Route path="/" element={<LayoutAuth />}>
-          <Route index element={<Login/>} />
-      </Route>
-      <Route path="/register" element={<LayoutAuth />}>
-          <Route index element={<Register/>} />
-      </Route>
-      
-      {/* Admin Routes */}
-      <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <LayoutAdmin />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          
-          {/* Mahasiswa Routes */}
-          <Route path="mahasiswa" element={<Mahasiswa />} />
-          <Route path="mahasiswa/:id" element={<MahasiswaDetail/>} />
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/" element={<LayoutAuth />}>
+            <Route index element={<Login />} />
+          </Route>
+          <Route path="/register" element={<LayoutAuth />}>
+            <Route index element={<Register />} />
+          </Route>
 
-          {/* Dosen Routes */}
-          <Route path="dosen" element={<Dosen />} />
-          <Route path="dosen/:id" element={<DosenDetail/>} />
+          {/* Admin Routes */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <LayoutAdmin />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
 
-          {/* Matkul Routes */}
-          <Route path="matkul" element={<Matkul />} />
-          <Route path="matkul/:id" element={<MatkulDetail/>} />
+            {/* Mahasiswa Routes */}
+            <Route path="mahasiswa" element={<Mahasiswa />} />
+            <Route path="mahasiswa/:id" element={<MahasiswaDetail />} />
 
-          {/* User Routes */}
-          <Route path="user" element={<User />} />
-          <Route path="user/:id" element={<UserDetail/>} />
+            {/* Dosen Routes */}
+            <Route path="dosen" element={<Dosen />} />
+            <Route path="dosen/:id" element={<DosenDetail />} />
 
-          <Route path="rencana-studi" element={<RencanaStudi />} />
-        </Route>
-    </Routes>
-    </BrowserRouter>
+            {/* Matkul Routes */}
+            <Route path="matkul" element={<Matkul />} />
+            <Route path="matkul/:id" element={<MatkulDetail />} />
 
-    
+            {/* User Routes */}
+            <Route path="user" element={<User />} />
+            <Route path="user/:id" element={<UserDetail />} />
+
+            <Route path="rencana-studi" element={<RencanaStudi />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
