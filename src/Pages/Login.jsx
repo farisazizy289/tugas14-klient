675d9a69973746984164e9a9745917d97c457eaf
farisazizy289 <farisazizy57@gmail.com>
2025-06-29 04:44:28 +0700

    memperbaiki typo import 3

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,44 +1,43 @@
-import { useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
-import Form from './Auth/Components/Form'; // Pastikan path-nya benar
-import { toastError, toastSuccess } from '../Utils/Helpers/ToastHelpers';
+import Form from "./Auth/Components/form"; // Pastikan path-nya benar
+import { toastError, toastSuccess } from "../Utils/Helpers/ToastHelpers";
 import { login } from "../Utils/Apis/AuthApi";
-import { useAuthStateContext } from './Context/AuthContext';
+import { useAuthStateContext } from "./Context/AuthContext";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const { user, setUser } = useAuthStateContext();
 
   if (user) return <Navigate to="/admin" />;
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // const { email } = email;
+    // const { password } = password;
 
- const handleLogin = async (e) => {
-  e.preventDefault();
-  // const { email } = email;
-  // const { password } = password;
-
-  try {
-    const user = await login(email, password);
-    setUser(user); // ini akan simpan ke context + localStorage
-    localStorage.setItem("user", JSON.stringify(user));
-    toastSuccess("Login berhasil");
-   setTimeout(() => {
-      navigate("/admin/dashboard");
-    }, 10);
-  } catch (err) {
-    toastError(err.message);
-  }
-};
+    try {
+      const user = await login(email, password);
+      setUser(user); // ini akan simpan ke context + localStorage
+      localStorage.setItem("user", JSON.stringify(user));
+      toastSuccess("Login berhasil");
+      setTimeout(() => {
+        navigate("/admin/dashboard");
+      }, 10);
+    } catch (err) {
+      toastError(err.message);
+    }
+  };
 
   return (
     <>
       <h2 className="text-3xl font-semibold text-center text-blue-600 mb-6">
         Login
       </h2>
-      <Form 
+      <Form
         onSubmit={handleLogin}
         email={email}
         setEmail={setEmail}
